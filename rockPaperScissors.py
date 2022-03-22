2388bb32b0eae3c6a8dd857a3338450dd0e02c6e
Shibli2316 <arshibli19@gmail.com>
2022-03-22 11:57:48 +0530

    Adding the rock paper scissor game

---
+++ rockPaperScissors.py
@@ -0,0 +1,6 @@
+import random
+randNumb=random.randint(1,3)
+print("\tWelcome to the basic game of rock paper scissor")
+print("\tTo play the game press 1")
+print("\tTo exit press 2")
+userInput=int(input("Enter your choise: "))

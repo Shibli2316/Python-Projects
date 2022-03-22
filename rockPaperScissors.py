33b1be0552e84162e253587bd58ce1f6033ee613
Shibli2316 <84792951+Shibli2316@users.noreply.github.com>
2022-03-22 11:59:40 +0530

    Update rockPaperScissors.py

--- rockPaperScissors.py
+++ rockPaperScissors.py
@@ -4,3 +4,7 @@ print("\tWelcome to the basic game of rock paper scissor")
 print("\tTo play the game press 1")
 print("\tTo exit press 2")
 userInput=int(input("Enter your choise: "))
+if userInput==1:
+  pass
+else:
+  exit()

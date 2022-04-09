56593612c50ae97c4b52191c8b4c06e53ffc78cc
Shibli2316 <84792951+Shibli2316@users.noreply.github.com>
2022-04-09 13:55:42 +0530

    Working with PDF

--- work_pdf.py
+++ work_pdf.py
@@ -1,6 +1,6 @@
 import PyPDF2
 
-
+# This will only work if you have the file in the directory if not use the write method
 new_pdf = open('intro.pdf', 'rb')
 reading_pdf = PyPDF2.PdfFileReader(new_pdf)
 print(reading_pdf.numPages)

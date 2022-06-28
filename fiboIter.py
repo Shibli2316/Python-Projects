3aa470137b24712ba0861a71a47fcef6f1fde3d7
Shibli2316 <84792951+Shibli2316@users.noreply.github.com>
2022-06-29 00:03:56 +0530

    Update fiboIter.py
    Completed the function

--- fiboIter.py
+++ fiboIter.py
@@ -1,2 +1,7 @@
-def fiboIter():
-    pass
+def fibonacci(n):
+    a,b = 0,1
+    for i in range(n):
+        a,b = b,a+b
+    return a
+user=int(input("Enter the fibonposition you want"))
+print(fibonacci(user))
